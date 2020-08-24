3d9a9627af3135b698eb8a9400403cc0fbda2ef6
Alexandru Burghelea <burghelea.alex@gmail.com>
2020-08-24 09:40:41 +0300

    update the js file

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-'use strict'
+"use strict";
 
 const videoElement = document.getElementById("video");
-const button = document.getElementById('button');
-const buttonChose = document.getElementById('choose');
+const button = document.getElementById("button");
+const buttonChose = document.getElementById("choose");
 
-
-//Prompt to seelct media stream, pass to video element
-
-async function selectMediaStream(){
-    try{
-        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-        videoElement.srcObject= mediaStream;
-        videoElement.onloadedmetadata = () => {
-            videoElement.play();
-        }
-        button.hidden = false;   
-
-    }catch(error){
-        //
-        console.log('whoops. error here:',error);
-    }    
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+    button.hidden = false;
+  } catch (error) {
+    //
+    console.log("whoops. error here:", error);
+  }
 }
 
-button.addEventListener('click',async ()=>{
-    button.disabled = true;
-    await videoElement.requestPictureInPicture();
-    button.disabled = false;
+button.addEventListener("click", async () => {
+  button.disabled = true;
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
 });
 
-
-buttonChose.addEventListener('click', selectMediaStream);
-
+buttonChose.addEventListener("click", selectMediaStream);
