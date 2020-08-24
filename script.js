49ff1fca2322c0d2895e7b9cbf338a38865406dd
Alexandru Burghelea <burghelea.alex@gmail.com>
2020-08-24 08:27:10 +0300

    updated the scrpit file

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('testing');
+'use strict'
+
+const videoElement = document.getElementById("video");
+const button = document.getElementById('button');
+
+
+//Prompt to seelct media stream, pass to video element
+
+async function selectMediaStream(){
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject= mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }   
+
+    }catch(error){
+        //
+        console.log('whoops. error here:',error);
+    }    
+}
+
+button.addEventListener('click',async ()=>{
+    button.disabled = true;
+    await videoElement.requestPictureInPicture();
+    button.disabled = false;
+});
+
+//On Load
+selectMediaStream();
